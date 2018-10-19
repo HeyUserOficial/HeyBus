666464e789905e52a6a5efed65a53bd666882052
yuri <yury@>
2018-10-19 18:50:08 -0300

    Novos arquivos do banco

---
+++ HeyBus/BD/HeyBus .sql
@@ -0,0 +1,120 @@
+/*drop database if exists HeyBus;
+create database if not exists HeyBus;
+use HeyBus;
+create table if not exists Acesso(
+id_Acesso int auto_increment not null,
+login_Acesso varchar(25),
+senha_Acesso varchar(25),
+nivel_Acesso char(15),
+primary key (id_Acesso)
+)ENGINE = innodb;
+
+ create table if not exists Cliente(
+id_Cliente int auto_increment not null,
+cpf_Cliente char(14),
+nome_Cliente varchar(70),
+nascimento_Cliente datetime,
+tel_Cliente char(20),
+cel_Cliente char(20),
+email_Cliente varchar(60),
+usuario_Cliente varchar(25),
+senha_Cliente varchar(25),
+primary key (id_Cliente)
+) ENGINE = innodb;
+
+create table if not exists Funcionario(
+id_Funcionario int auto_increment not null,
+cpf_Funcionario char(14),
+nome_Funcionario varchar(70),
+email_Funcionario varchar(60),
+endereco_Funcionario varchar(100),
+id_Acesso int,
+primary key (id_Funcionario)
+)ENGINE = innodb;
+alter table Funcionario
+add foreign key(id_Acesso)
+references Acesso(id_Acesso);
+
+create table if not exists Rota(
+id_Rota int auto_increment not null,
+origem_Rota varchar(60),
+destino_Rota varchar(60),
+itinerario_Rota time,
+distancia_Rota char(10),
+primary key(id_Rota)
+)ENGINE = innodb;
+
+create table if not exists Bancos(
+id_Bancos int auto_increment not null,
+num_Banco int,
+primary key(id_Bancos)
+)ENGINE = innodb;
+
+create table if not exists Onibus(
+id_Onibus int auto_increment not null,
+viacao_Onibus varchar(30),
+categoria_Onibus varchar(30),
+id_Bancos int,
+manutencao_Onibus char(15),
+primary key(id_Onibus)
+)ENGINE = innodb;
+alter table Onibus
+add foreign key(id_Bancos)
+references Bancos(id_Bancos);
+
+create table if not exists Viagem(
+id_Viagem int auto_increment not null,
+id_Rota int,
+id_Onibus int,
+data_Viagem date,
+valor_Viagem decimal(3,2),
+primary key(id_Viagem)
+)ENGINE = innodb;
+alter table Viagem 
+add foreign key(id_Rota)
+references Rota(id_Rota);
+alter table Viagem 
+add foreign key(id_Onibus)
+references Onibus(id_Onibus);
+
+create table if not exists FormaPagamento(
+id_FormPag int auto_increment not null,
+nome_FormPag char(15),
+primary key(id_FormPag)
+)ENGINE = innodb;
+ 
+create table if not exists Passagem(
+id_Passagem int auto_increment not null,
+id_Cliente int,
+id_Viagem int,
+id_FormPag int,
+cpf_Cliente char(14),
+desconto_Passagem decimal(3,2),
+valor_Passagem decimal(3,2),
+data_Compra datetime,
+primary key(id_Passagem)
+)ENGINE = innodb;
+alter table Passagem
+add foreign key(id_Cliente)
+references Cliente(id_Cliente);
+alter table Passagem
+add foreign key(id_Viagem)
+references Viagem(id_Viagem);
+alter table Passagem
+add foreign key(id_FormPag)
+references FormaPagamento(id_FormPag);
+
+create table if not exists Cartao(
+id_Cartao int auto_increment not null,
+num_Cartao int,
+val_Cartao date,
+nome_Cartao varchar(50),
+bandeira_Cartao char(40),
+codigo_Seg int,
+primary key (id_Cartao)
+)ENGINE = innodb;
+alter table Cartao
+add column id_Cliente int;
+alter table Cartao
+add foreign key(id_Cliente)
+references Cliente(id_Cliente);
---
+++ HeyBus/BD/StoredProcedures.sql
@@ -0,0 +1,242 @@
+/*Use HeyBus;
+
+/*<----------------Acesso-------------->*/
+Delimiter $$ 
+create Procedure SP_Cadastrar_Acesso
+(in login varchar(25), in senha varchar(25), in nivel char(15))
+begin
+	insert into Acesso (login_Acesso, senha_Acesso, nivel_Acesso) 
+    values(login, senha, nivel);
+end $$
+Delimiter ;
+
+Delimiter $$
+create Procedure SP_Alterar_Acesso
+(in id int, in login varchar(25), in senha varchar(25))
+begin
+	update Acesso set
+					 login_Acesso = login,
+					 senha_Acesso = senha
+	where
+					 id_Acesso = id;
+end $$
+Delimiter ;
+
+Delimiter $$
+create procedure SP_Efetuar_Acesso
+(in login varchar(25), in senha varchar(25))
+begin 
+	 select * from Acesso where login_Acesso = login and senha_Acesso = senha;
+end $$
+Delimiter ;
+
+/*<----------------Cliente-------------->*/
+Delimiter $$ 
+create Procedure SP_Cadastrar_Cliente
+(in cpf char(14),in nome varchar(70),in nascimento date,in tel char(20),in cel char(20),in email varchar(60),
+ in usuario varchar(25), in senha varchar(25))
+begin 
+	insert into Cliente (cpf_Cliente, nome_Cliente, nascimento_Cliente, tel_Cliente, cel_Cliente,
+    email_Cliente, usuario_Cliente, senha_Cliente) values (cpf, nome, nascimento, tel, cel, email, usuario, senha);
+end $$
+Delimiter ;
+
+Delimiter $$ 
+create procedure SP_Login_Cliente
+(in usuario varchar(25), in senha varchar(25))
+begin 
+	 Select usuario_Cliente, senha_Cliente from Cliente
+     where
+		  usuario_Cliente = usuario 
+          and 
+          senha_Cliente = senha;
+end 
+Delimiter ;
+
+Delimiter $$ 
+create Procedure SP_Atualizar_Cliente
+(in id int, in cpf char(14), in nome varchar(70), in nascimento datetime, in tel char(20), in cel char(20), in email varchar(60),
+in usuario varchar(25), senha varchar(25))
+begin 
+	update Cliente set 
+					  cpf_Cliente = cpf,
+					  nome_Cliente = nome,
+                      nascimento_Cliente = nascimento,
+                      tel_Cliente = tel,
+                      cel_Cliente = cel,
+                      email_Cliente = email,
+                      usuario_Cliente = usuario,
+                      senha_Cliente = senha
+	where 
+					  id_Cliente = id;
+end $$
+Delimiter ;
+
+Delimiter $$
+create Procedure SP_Deletar_Cliente
+(in id int)
+begin 
+	delete from Cliente where id_Cliente = id;
+end $$
+Delimiter ;
+
+Delimiter $$
+create Procedure SP_Consultar_Cliente
+(in id int)
+begin 
+	select * from Cliente where id_Cliente = id;
+end $$
+Delimiter ;
+
+/*<----------------Funcionário-------------->*/
+Delimiter $$ 
+create procedure SP_Cadastrar_Func
+(in cpf char(14), in nome varchar(70), in email varchar(60), in endereco varchar(100), in acesso int)
+begin 
+	insert into Funcionario(cpf_Funcionario, nome_Funcionario, email_Funcionario, endereco_Funcionario, id_Acesso)
+	values(cpf, nome, email, endereco, acesso);
+end $$
+Delimiter ;
+
+Delimiter $$
+create procedure SP_Alterar_Func
+(in id int, in cpf char(14), in nome varchar(70), in email varchar(60), in endereco varchar(100))
+begin 
+	 update Funcionario set
+						   cpf_Funcionario = cpf,
+						   nome_Funcionario = nome,
+						   email_Funcionario = email,
+						   endereco_Funcionario = endereco
+	where
+						   id_Funcionario = id;
+end $$
+Delimiter ;
+
+Delimiter $$
+create procedure SP_Consultar_Func
+(in id int)
+begin
+	select cpf_Funcionario, nome_Funcionario, email_Funcionario, endereco_Funcionario, 
+	Acesso.login_Acesso, Acesso.senha_Acesso from Funcionario inner join Acesso
+	on Funcionario.id_Acesso = Acesso.id_Acesso where id_Funcionario = id;
+end $$
+Delimiter ;
+
+/*<----------------Rota-------------->*/
+Delimiter $$ 
+create procedure SP_Cadastrar_Rota
+(in origem varchar(60), in destino varchar(60), in itinerario time, in distancia char(10))
+begin
+	insert into Rota(origem_Rota, destino_Rota, itinerario_Rota, distancia_Rota)
+    values(origem, destino, itinerario, distancia);
+end $$
+Delimiter ;
+
+Delimiter $$
+create procedure SP_Consultar_Rota
+(in id int)
+begin
+	 Select * from Rota where id_Rota = id;
+end $$
+Delimiter ;
+
+Delimiter $$
+create procedure SP_Alterar_Rota
+(in id int, in origem varchar(60), in destino varchar(60), in itinerario time, in distancia char(10))
+begin
+	 update Rota set 
+					origem_Rota = origem,
+                    destino_Rota = destino, 
+                    itinerario_Rota = itinerario, 
+                    distancia_Rota = distancia
+	 where
+					id_Rota = id;
+end $$
+Delimiter ;
+
+/*<----------------Ônibus-------------->*/
+Delimiter $$
+create procedure SP_Cadastrar_Onibus
+(in viacao varchar(30), in categoria varchar(30), in bancos int, in manutencao char(15))
+begin
+	 insert into Onibus(viacao_Onibus, categoria_Onibus, id_Bancos, manutencao_Onibus)
+     values(viacao, categoria, bancos, manutencao);
+end $$ 
+Delimiter ;
+
+Delimiter $$
+create procedure SP_Consultar_Onibus
+(in id int)
+begin 
+	 Select * from Onibus where id_Onibus = id;
+end $$
+Delimiter ;
+
+Delimiter $$
+create procedure SP_Alterar_Onibus
+(in id int, in manutencao char(15))
+begin
+	update Onibus set 
+					 manutencao_Onibus = manutencao
+	where 
+					 id_Onibus = id;
+end $$
+Delimiter ;
+
+
+/*<----------------Viagem-------------->*/
+Delimiter $$
+create procedure SP_Cadastrar_Viagem
+(in rota int, in onibus int, in dataVi date, in valor decimal(3,2))
+begin
+     insert into Viagem(id_Rota, id_Onibus, data_Viagem, valor_Viagem)
+     values(rota, onibus, dataVi, valor);
+end $$
+Delimiter ;
+
+
+Delimiter $$ 
+create procedure SP_Consultar_Viagem
+(in id int)
+begin 
+	 Select destino_Rota.Rota, viacao_Onibus.Onibus, categoria_Onibus.Onibus, data_Viagem, valor_Viagem,
+     itinerario_Rota.Rota, distancia_Rota.Rota, id_Bancos.Onibus
+     from Viagem 
+     inner join Rota
+     on Viagem.id_Rota = Rota.id_Rota 
+     inner join Onibus 
+     on Viagem.id_Onibus = Onibus.id_Onibus
+     where id_Viagem = id;
+end $$
+Delimiter ; 
+
+Delimiter $$
+create procedure SP_Alterar_Viagem
+(in id int, in dataVi datetime, valor decimal(3,2))
+begin
+	 update Viagem set 
+					  data_Viagem_Viagem = dataVi,
+                      valor_Viagem = valor
+	 where
+					  id_Viagem = id;
+end $$
+Delimiter ;
+
+Delimiter $$
+create procedure SP_Deletar_Viagem
+(in id int)
+begin 
+	 delete from Viagem 
+     where id_Viagem = id;
+end $$
+Delimiter ;
+
+/*<----------------Passagem-------------->*/
+Delimiter $$
+create procedure SP_Cadastrar_Passagem
+(in cli int, in viagem int, in formPag int, in cpf char(14), in desconto decimal(3,2), in valor decimal(3,2), in dataCompra datetime)
+begin 
+	 insert into Passagem(id_Cliente, id_Viagem, id_FormPag, cpf_Cliente, desconto_Passagem, valor_Passagem, data_Compra)
+     values(cli, viagem, formPag, cpf, desconto, valor, dataCompra);
+end $$
+Delimiter ;
