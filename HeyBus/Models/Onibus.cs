051f1941bb8b7e1ba8d55757f98a1959c2fef9c2
HeyUserOficial <heyuseroficial@gmail.com>
2018-10-24 17:05:39 -0300

    "Repositório do ônibus em progresso"

--- HeyBus/Models/Onibus.cs
+++ HeyBus/Models/Onibus.cs
@@ -9,42 +9,18 @@ namespace HeyBus.Models
     public class Onibus
     {
         [Key]
-        private int id_Onibus;
-        public int id_Oni
-        {
-            get { return id_Onibus; }
-            set { id_Onibus = id_Oni; }
-        }
+        public int id_Onibus { get; set; }
 
-        [MaxLength(30), Required]
-        private string viacao_Onibus;
-        public string viacao_Oni
-        {
-            get { return viacao_Onibus; }
-            set { viacao_Onibus = viacao_Oni; }
-        }
+        [Display (Name = "Viação"), MaxLength(30), Required]
+        public string viacao_Onibus { get; set; }
 
-        [MaxLength(30), Required]
-        private string categoria_Onibus;
-        public string categoria_Oni
-        {
-            get { return categoria_Onibus; }
-            set { categoria_Onibus = categoria_Oni; }
-        }
+        [Display (Name = "Categôria"), MaxLength(30), Required]
+        public string categoria_Onibus { get; set; }
 
-        
-        private List<int> assentos_Onibus;
-        public List<int> assentos_Oni
-        {
-            get { return assentos_Onibus; }
-            set { assentos_Onibus = assentos_Oni; }
-        }
+        [Display (Name = "Bancos")]
+        public int assentos_Onibus { get; set; }
 
-        private string manutencao_Onibus;
-        public string manutencao_Oni
-        {
-            get { return manutencao_Onibus; }
-            set { manutencao_Onibus = manutencao_Oni; }
-        }
+        [Display (Name = "Manutenção"), MaxLength(15), Required]
+        public string manutencao_Onibus { get; set; }
     }
 }
---
+++ HeyBus/Repository/RepositoryOnibus.cs
@@ -0,0 +1,72 @@
+﻿using HeyBus.Connection;
+using HeyBus.Models;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HeyBus.Repository
+{
+    public class RepositoryOnibus
+    {
+        MySqlCommand cmd;
+        MySqlDataReader dr;
+        Conexao conn = new Conexao();
+
+        public void Cadastrar_Onibus(Onibus oni)
+        {
+            try
+            {
+                conn.abrirConexao();
+                cmd = new MySqlCommand("SP_Cadastrar_Onibus", Conexao.conexao);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@viacao", oni.viacao_Onibus);
+                cmd.Parameters.AddWithValue("@categoria", oni.categoria_Onibus);
+                cmd.Parameters.AddWithValue("@banco", oni.assentos_Onibus);
+                cmd.Parameters.AddWithValue("@manutencao", oni.manutencao_Onibus);
+            }
+            catch (Exception kj)
+            {
+                throw;
+            }
+            finally
+            {
+                conn.fecharConexao();
+            }
+        }
+
+        public void Consultar_Onibus(Onibus oni)
+        {
+            try
+            {
+                conn.abrirConexao();
+                cmd = new MySqlCommand("SP_Consultar_Onibus", Conexao.conexao);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id", oni.id_Onibus);
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    oni.viacao_Onibus = dr["viacao_Onibus"].ToString();
+                    oni.categoria_Onibus = dr["categoria_Onibus"].ToString();
+                    oni.assentos_Onibus = Convert.ToInt32(dr["id_Banco"].ToString());
+                    oni.manutencao_Onibus = dr["manutencao_Onibus"].ToString();
+                }
+            }
+            catch (Exception mk)
+            {
+                throw;
+            }
+            finally
+            {
+                conn.fecharConexao();
+            }
+        }
+
+        public void Update_Onibus(Onibus oni)
+        {
+
+        }
+    }
+}
