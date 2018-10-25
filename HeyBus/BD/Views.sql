dac872b012881c47ddf2585a5ea21a7bcb9714e0
3B Modular Info <3binfo@basilides.local>
2018-10-25 17:22:30 -0300

    Script das views atualizada

--- HeyBus/BD/Views.sql
+++ HeyBus/BD/Views.sql
@@ -4,3 +4,46 @@ select cpf_Funcionario, nome_Funcionario, email_Funcionario, endereco_Funcionari
 Acesso.login_Acesso from Funcionario inner join Acesso
 on Funcionario.id_Acesso = Acesso.id_Acesso;
 */
+
+/*Create View Consultar_Clientes as
+select * from Cliente;
+*/
+
+/*Create View Consultar_Rotas as 
+select * from Rota;
+*/
+
+/*Create View Consultar_Onibus
+select * from Onibus;*/
+
+/*Create View Consultar_Viagens as
+Select destino_Rota.Rota, viacao_Onibus.Onibus, categoria_Onibus.Onibus, data_Viagem, valor_Viagem,
+     itinerario_Rota.Rota, distancia_Rota.Rota, id_Bancos.Onibus
+     from Viagem 
+     inner join Rota
+     on Viagem.id_Rota = Rota.id_Rota 
+     inner join Onibus 
+     on Viagem.id_Onibus = Onibus.id_Onibus;
+*/
+
+/*Create View Consultar_Passagens as
+	 select nome_Cliente.Cliente, origem_Rota.Rota, viacao_Onibus.Onibus, destino_Rota.Rota, data_Viagem.Viagem, nome_FormPag.FormaPagamento, cpf_Cliente, desconto_Passagem,
+     valor_Passagem, data_Compra
+     from Passagem 
+     inner join Cliente
+     on 
+     Passagem.id_Cliente = Cliente.id_Cliente
+     inner join Rota
+     on
+     Passagem.id_Rota = Rota.id_Rota
+     inner join Onibus
+     on 
+     Passagem.id_Onibus = Onibus.id_Onibus
+     inner join Viagem
+     on 
+     Passagem.id_Viagem = Viagem.id_Viagem
+     inner join FormaPagamento
+     on
+     Passagem.id_FormPag = FormaPagamento.id_FormPag;
+	*/
+
