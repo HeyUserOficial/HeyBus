bd769b5a6b85000961ad19d8836a8d706cfafc50
Leo Saglia <leosaglia@hotmail.com>
2018-12-02 15:40:21 -0200

    Adionando busca de passagens em outras paginas

--- HeyBus/Controllers/HomeController.cs
+++ HeyBus/Controllers/HomeController.cs
@@ -58,17 +58,104 @@ namespace HeyBus.Controllers
 
             return View();
         }
+        [HttpPost]
+        public ActionResult Sobre(Viagem v)
+        {
+            if (v.data_Volta == Convert.ToDateTime("01/01/0001") && v.data_Ida == Convert.ToDateTime("01/01/0001") && v.rot.origem_Rota != null && v.rot.destino_Rota != null)
+            {
+                repViagem.PesquisarViagemCompleto(v.data_Ida, v.rot.origem_Rota, v.rot.destino_Rota, v.data_Volta);
+                Response.Redirect("~/Viagens/BuscarViagemCompleta?dataPartida=" + v.data_Ida.ToString("MM/dd/yyyy") + "&origem=" + v.rot.origem_Rota + "&Destino=" + v.rot.destino_Rota + "&dataVolta=" + v.data_Volta.ToString("MM/dd/yyyy"));
+            }
+            else if (v.data_Volta == null && v.data_Ida == null && v.rot.origem_Rota != null && v.rot.destino_Rota != null)
+            {
+                repViagem.PesquisarViagemDestino(v.rot.origem_Rota, v.rot.destino_Rota);
+                Response.Redirect("~/Viagens/BuscarViagemDestino?origem=" + v.rot.origem_Rota + "&Destino=" + v.rot.destino_Rota);
+            }
+            else if (v.data_Volta != null && v.data_Ida != null && v.rot.origem_Rota != null && v.rot.destino_Rota != null)
+            {
+                repViagem.PesquisarViagemCompleto(v.data_Ida, v.rot.origem_Rota, v.rot.destino_Rota, v.data_Volta);
+                Response.Redirect("~/Viagens/BuscarViagemCompleta?dataPartida=" + v.data_Ida.ToString("MM/dd/yyyy") + "&origem=" + v.rot.origem_Rota + "&Destino=" + v.rot.destino_Rota + "&dataVolta=" + v.data_Volta.ToString("MM/dd/yyyy"));
+            }
+            else if (v.data_Volta == null)
+            {
+                repViagem.PesquisarViagemIda(v.data_Ida, v.rot.origem_Rota, v.rot.destino_Rota);
+                Response.Redirect("~/Viagens/BuscarViagemIda?dataPartida=" + v.data_Ida.ToString("MM/dd/yyyy") + "&origem=" + v.rot.origem_Rota + "&Destino=" + v.rot.destino_Rota);
+            }
+            else
+            {
+                Response.Redirect("~/Viagens/");
+            }
+            return View(v);
+        }
 
         public ActionResult Servicos()
         {
             ViewBag.Message = "Your service description page.";
             return View();
         }
+        [HttpPost]
+        public ActionResult Servicos(Viagem v)
+        {
+            if (v.data_Volta == Convert.ToDateTime("01/01/0001") && v.data_Ida == Convert.ToDateTime("01/01/0001") && v.rot.origem_Rota != null && v.rot.destino_Rota != null)
+            {
+                repViagem.PesquisarViagemCompleto(v.data_Ida, v.rot.origem_Rota, v.rot.destino_Rota, v.data_Volta);
+                Response.Redirect("~/Viagens/BuscarViagemCompleta?dataPartida=" + v.data_Ida.ToString("MM/dd/yyyy") + "&origem=" + v.rot.origem_Rota + "&Destino=" + v.rot.destino_Rota + "&dataVolta=" + v.data_Volta.ToString("MM/dd/yyyy"));
+            }
+            else if (v.data_Volta == null && v.data_Ida == null && v.rot.origem_Rota != null && v.rot.destino_Rota != null)
+            {
+                repViagem.PesquisarViagemDestino(v.rot.origem_Rota, v.rot.destino_Rota);
+                Response.Redirect("~/Viagens/BuscarViagemDestino?origem=" + v.rot.origem_Rota + "&Destino=" + v.rot.destino_Rota);
+            }
+            else if (v.data_Volta != null && v.data_Ida != null && v.rot.origem_Rota != null && v.rot.destino_Rota != null)
+            {
+                repViagem.PesquisarViagemCompleto(v.data_Ida, v.rot.origem_Rota, v.rot.destino_Rota, v.data_Volta);
+                Response.Redirect("~/Viagens/BuscarViagemCompleta?dataPartida=" + v.data_Ida.ToString("MM/dd/yyyy") + "&origem=" + v.rot.origem_Rota + "&Destino=" + v.rot.destino_Rota + "&dataVolta=" + v.data_Volta.ToString("MM/dd/yyyy"));
+            }
+            else if (v.data_Volta == null)
+            {
+                repViagem.PesquisarViagemIda(v.data_Ida, v.rot.origem_Rota, v.rot.destino_Rota);
+                Response.Redirect("~/Viagens/BuscarViagemIda?dataPartida=" + v.data_Ida.ToString("MM/dd/yyyy") + "&origem=" + v.rot.origem_Rota + "&Destino=" + v.rot.destino_Rota);
+            }
+            else
+            {
+                Response.Redirect("~/Viagens/");
+            }
+            return View(v);
+        }
 
         public ActionResult Onibus()
         {
             ViewBag.Message = "Your bus service description page.";
             return View();
+        }
+        [HttpPost]
+        public ActionResult Onibus(Viagem v)
+        {
+            if (v.data_Volta == Convert.ToDateTime("01/01/0001") && v.data_Ida == Convert.ToDateTime("01/01/0001") && v.rot.origem_Rota != null && v.rot.destino_Rota != null)
+            {
+                repViagem.PesquisarViagemCompleto(v.data_Ida, v.rot.origem_Rota, v.rot.destino_Rota, v.data_Volta);
+                Response.Redirect("~/Viagens/BuscarViagemCompleta?dataPartida=" + v.data_Ida.ToString("MM/dd/yyyy") + "&origem=" + v.rot.origem_Rota + "&Destino=" + v.rot.destino_Rota + "&dataVolta=" + v.data_Volta.ToString("MM/dd/yyyy"));
+            }
+            else if (v.data_Volta == null && v.data_Ida == null && v.rot.origem_Rota != null && v.rot.destino_Rota != null)
+            {
+                repViagem.PesquisarViagemDestino(v.rot.origem_Rota, v.rot.destino_Rota);
+                Response.Redirect("~/Viagens/BuscarViagemDestino?origem=" + v.rot.origem_Rota + "&Destino=" + v.rot.destino_Rota);
+            }
+            else if (v.data_Volta != null && v.data_Ida != null && v.rot.origem_Rota != null && v.rot.destino_Rota != null)
+            {
+                repViagem.PesquisarViagemCompleto(v.data_Ida, v.rot.origem_Rota, v.rot.destino_Rota, v.data_Volta);
+                Response.Redirect("~/Viagens/BuscarViagemCompleta?dataPartida=" + v.data_Ida.ToString("MM/dd/yyyy") + "&origem=" + v.rot.origem_Rota + "&Destino=" + v.rot.destino_Rota + "&dataVolta=" + v.data_Volta.ToString("MM/dd/yyyy"));
+            }
+            else if (v.data_Volta == null)
+            {
+                repViagem.PesquisarViagemIda(v.data_Ida, v.rot.origem_Rota, v.rot.destino_Rota);
+                Response.Redirect("~/Viagens/BuscarViagemIda?dataPartida=" + v.data_Ida.ToString("MM/dd/yyyy") + "&origem=" + v.rot.origem_Rota + "&Destino=" + v.rot.destino_Rota);
+            }
+            else
+            {
+                Response.Redirect("~/Viagens/");
+            }
+            return View(v);
         }
 
         public ActionResult Duvidas()
